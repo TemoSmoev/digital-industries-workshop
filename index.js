b1a6800c9c39d6aedd6048a5d3d4cc8f5576a59b
temo <temo@gmail.com>
2022-12-17 13:35:59 +0400

    workshop-7

--- index.js
+++
@@ -1,8 +0,0 @@
-function Phone(manufacturer, model, year) {
-  this.manufacturer = manufacturer;
-  this.model = model;
-  this.year = year;
-  this.ring = function () {
-    console.log("this phone is ringing");
-  };
-}

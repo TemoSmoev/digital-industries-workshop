fc724c0151d96ab44d338103cd816d66c8ab137f
temo <temo@gmail.com>
2023-01-27 17:46:47 +0400

    loops

---
+++ index.js
@@ -0,0 +1,9 @@
+// const a = { a: 34, b: 90, c: { name: "John", age: 55 } };
+// const newC = { ...a.c };
+// const newA = { ...a };
+// newA.c = newC;
+
+// newA.c.surname = "Doe";
+
+// console.log(a);
+// console.log(newA);
